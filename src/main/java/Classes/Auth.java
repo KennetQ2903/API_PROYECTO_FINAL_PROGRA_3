70f3f318409f105b185160dadeec75acdb4ccf89
KennetQ2903 <kennetquiroz@gmail.com>
2023-05-28 20:55:59 -0600

    add auth class

---
+++ src/main/java/Classes/Auth.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Auth {
+    private boolean auth = false;
+
+    public boolean isAuth() {
+        return auth;
+    }
+
+    public void setAuth(boolean auth) {
+        this.auth = auth;
+    }
+}
